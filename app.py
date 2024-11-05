44f26d3b633152ab51ec889df8ed1f44f265d383
ZEE <white.noise616@gmail.com>
2024-11-04 18:24:11 -0700

    added allowed origins

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ allowed_origins = os.getenv('ALLOWED_ORIGINS', '').split(',')
 allowed_origins = [origin.strip() for origin in allowed_origins if origin.strip()]
 
 app = Flask(__name__)
-# CORS(app, resources={r"/*": {"origins": allowed_origins}})  
-CORS(app)  
+CORS(app, resources={r"/*": {"origins": allowed_origins}})  
+# CORS(app)  
 # Add Cloudinary configuration after Flask initialization
 cloudinary.config(
     cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
